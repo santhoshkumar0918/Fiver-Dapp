0088c640d2e34ee6bcd04198ba7396c274e63b2f
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2024-10-12 20:15:29 +0530

    added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,6 @@
 import express from "express"
 
 const app = express();
+
+// postgre + prisma => ORM
+// postgresql://neondb_owner:dpgv1AuG8rBH@ep-cool-tooth-a5hshdcm.us-east-2.aws.neon.tech/neondb?sslmode=require
